a0d717135ae6be12dba25d562f35533219f638a5
Keyshawn Jones <keyshawnjones29@gmail.com>
2023-03-28 13:53:09 -0500

    cleaned up spring boot

--- backend/src/main/java/com/kiwi/backend/RestAPI.java
+++ backend/src/main/java/com/kiwi/backend/RestAPI.java
@@ -1,15 +1,25 @@
 package com.kiwi.backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class RestAPI {
-    
+
     @GetMapping("/yes")
     public Person createPerson() {
         Person newPerson = new Person();
         newPerson.setName("Kiwi");
+        newPerson.setAge(16);
+        return newPerson;
+    }
+
+    @GetMapping("/nope")
+    public Person createPersons() {
+        Person newPerson = new Person();
+        newPerson.setName("Bellventus");
         newPerson.setAge(16);
         return newPerson;
     }
